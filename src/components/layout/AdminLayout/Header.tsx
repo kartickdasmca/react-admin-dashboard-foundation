d4ff43d7ecbba78f98ef48977d5631faf4444b20
kartickdasmca <kartickdasdk@gmail.com>
2026-09-14 14:19:56 +0530

    connect to hamburger

--- src/components/layout/AdminLayout/Header.tsx
+++ src/components/layout/AdminLayout/Header.tsx
@@ -1,7 +1,7 @@
 import { Menu,Search,Filter,Plus,Sun,Bell, Settings, ChevronDown} from 'lucide-react'
 import React from 'react'
-
-const Header = () => {
+import type {HeaderProps} from '../../../types/header'
+const Header = ({onSidebarToggle}:HeaderProps) => {
   return (
     <div className='bg-white/-80 dark:bg-slate-900/80 backdrop-blur-xl border-b
        border-slate-200/50 dark:border-slate-700/50 px-6 py-4'
@@ -12,6 +12,7 @@ const Header = () => {
                <button
                 className='p-2 rounded-lg text-slate-600 dark:text-slate-300
                 hover:bg-slate-100 dark:hover:bg-slate-800 transition-colors'
+                onClick={onSidebarToggle}
                >
                <Menu className='w-5 h-5'/>
               </button>
--- src/components/layout/AdminLayout/MainLayout.tsx
+++ src/components/layout/AdminLayout/MainLayout.tsx
@@ -7,7 +7,10 @@ import Header from "./Header";
 const MainLayout = () => {
   const [sideBarCollapsed, setSideBarCollapsed] =
     useState(false);
-
+    
+  const handleSidebarToggle = () => {
+    setSideBarCollapsed((prev) => !prev);
+  };
   return (
   <div className='min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 transition-all duration-500'>
     <div className="flex h-screen overflow-hidden">
@@ -19,7 +22,7 @@ const MainLayout = () => {
       />
 
       <div className="flex flex-1 flex-col overflow-hidden">
-        <Header />
+        <Header onSidebarToggle={handleSidebarToggle}/>
         <main className="flex-1 overflow-y-auto">
           <Outlet />
         </main>
